b174f589860e9ad89179e614627ec8c95ddbdd90
Daniel Arias <danielandres.98.da@gmail.com>
2024-11-18 13:34:47 -0300

    Config project.

---
+++ src/components/Rule/Rule.tsx
@@ -0,0 +1,9 @@
+import styles from './Rule.module.scss'
+
+const Rule = () => {
+  return (
+    <div><h1 className={styles['color-blue']}>PROBANDO SCSS</h1></div>
+  )
+}
+
+export default Rule;
---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,9 @@
+import styles from './Section.module.scss'
+
+const Section = () => {
+  return (
+    <div className={styles['color-red']}>TEST</div>
+  )
+}
+
+export default Section
