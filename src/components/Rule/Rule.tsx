f83382f6008897893664b8b56ac229d2c7131c5b
Daniel Arias <danielandres.98.da@gmail.com>
2024-11-23 12:22:24 -0300

    Added new steps styles.

--- src/components/Rule/Rule.tsx
+++ src/components/Rule/Rule.tsx
@@ -2,13 +2,14 @@ import styles from './Rule.module.scss'
 
 interface Rule {
   name: string,
+  isSelected: boolean,
   src: string,
 }
 
-export const Rule = ({ name, src }: Rule) => {
+export const Rule = ({ name, isSelected, src }: Rule) => {
   return (
-    <div className={`${styles['rule']} ${styles[name]}`}>
-      <div className={styles['rule-background']}>
+    <div className={`${styles['rule']} ${styles[name] || ''} ${isSelected ? styles['selected'] : ''}`}>
+      <div className={`${styles['rule-background']} ${src ? '' : styles['empty']}`}>
         <img src={src} className={styles['icon']}/>
       </div>
     </div>
--- src/components/RuleSelector/RuleSelector.tsx
+++ src/components/RuleSelector/RuleSelector.tsx
@@ -9,11 +9,25 @@ import BGTriangleSVG from '../../assets/bg-triangle.svg'
 export const RuleSelector = () => {
   return (
     <main className={styles['rule-selector']}>
-      <img className={styles['bg-triangle']} src={BGTriangleSVG}/>
+      {/* <img className={styles['bg-triangle']} src={BGTriangleSVG}/>
       <section className={styles['rule-selector-content']}>
         <Rule src={iconPaperSVG} name='paper'/>
         <Rule src={iconScissorSVG} name='scissor'/>
         <Rule src={iconRockSVG} name='rock'/>
+      </section> */}
+      <section className={styles['rule-selected']}>
+        <div className={styles['picked']}>
+          <h3 className={styles['title']}>You picked</h3>
+          <Rule src={iconPaperSVG} name='paper' isSelected/>
+        </div>
+        <div className={styles['result']}>
+          <h4>You lose</h4>
+          <button>Play again</button>
+        </div>
+        <div className={styles['picked']}>
+          <h3 className={styles['title']}>The house picked</h3>
+          <Rule isSelected/>
+        </div>
       </section>
     </main>
   )
